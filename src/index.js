688bede00dd00f8346a35887db860d4d02df6f15
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2020-02-04 17:30:01 +0100

    app.js, ; removed double robofriends folder

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 // import { robots } from './robots'; in 12.1m
 
-ReactDOM.render(<App/>
+ReactDOM.render(<App/>, document.getElementById('root'));
 		// <CardList robots = { robots }/> 12.1m
-	, document.getElementById('root'));
+	// , document.getElementById('root'));  -- samo go premestuvam
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
